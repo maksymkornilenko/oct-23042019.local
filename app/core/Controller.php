40fc57256e0b25d5badda088f7a1caf5a534d2cd
user <Global@mail.ua>
2019-04-23 19:37:21 +0300

    add view and model properties

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -3,6 +3,11 @@
 namespace core;
 
 abstract class Controller {
-
+    
+    protected $model;
+    protected $view;
+    public function __construct() {
+	$this->view=new View();
+    }
     abstract public function action_index();
 }
