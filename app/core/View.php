47a0c5d0d09ef7381ac5815526fe5389a334f853
user <Global@mail.ua>
2019-04-23 19:41:15 +0300

    add render method

--- app/core/View.php
+++ app/core/View.php
@@ -4,5 +4,7 @@
 namespace core;
 
 class View {
-    //put your code here
+    public function render($content_view,$template_view='template_main_view'){
+	include_once 'app'.DIRECTORY_SEPARATOR.'views'.DIRECTORY_SEPARATOR.$template_view.'.php';
+    }
 }
