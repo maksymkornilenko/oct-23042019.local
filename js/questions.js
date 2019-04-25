98abf4920d3c92f005f72be8b95cd17b0f2a8d4c
user <Global@mail.ua>
2019-04-25 20:00:45 +0300

    example pb api

--- js/questions.js
+++ js/questions.js
@@ -1,3 +1,38 @@
-alert('hello');
+document.getElementById('courses').onclick = function () {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5');
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            if (xhr.status = 200) {
+                var json_text = xhr.responseText;
+                var courses = JSON.parse(json_text);
+                for (var i = 0; i < courses.length; i++) {
+                    alert(courses[i].ccy);//TODO выводи как хочешь
+                }
+            }
+        }
+    };
+    xhr.send();
+};
 
+function getQuestions() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', '/api/questions');
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            if (xhr.status = 200) {
+                var json_text = xhr.responseText;
+                var questions = JSON.parse(json_text);
+                return questions;
+            }else{
+                return false;
+            }
+        }
+    };
+    xhr.send();
+}
 
+document.getElementById('questions-btn').onclick=function (){
+    var questions=getQuestions();
+    console.log(questions);
+}
